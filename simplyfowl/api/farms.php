db2b1bc65dcccd72daf279d72c7f1ddd5950f940
Brian Mendoza <brm21@comcast.net>
2019-10-10 07:44:54 -0400

    Add files via upload

---
+++ simplyfowl/api/farms.php
@@ -0,0 +1,57 @@
+<?php
+$host = "127.0.0.1"; 
+$user = "brian"; 
+$password = "MyNewPass"; 
+$dbname = "chickens"; 
+$id = '';
+
+$con = mysqli_connect($host, $user, $password,$dbname);
+
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+//$input = json_decode(file_get_contents('php://input'),true);
+
+
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+
+switch ($method) {
+    case 'GET':
+      $id = $_GET['id'];
+      $sql = "select * from Farm".($id?" where id=$id":''); 
+      break;
+    case 'POST':
+      $name = $_POST["farm_name"];
+      $address = $_POST["farm_address"];
+      $city = $_POST["farm_city"];
+      $building = $_POST["farm_bldg"];
+      
+
+      $sql = "insert into Farm (farm_name, farm_address, farm_city, farm_bldg) values ('$name', '$address', '$city', '$building')"; 
+      break;
+}
+
+// run SQL statement
+$result = mysqli_query($con,$sql);
+
+// die if SQL statement failed
+if (!$result) {
+  http_response_code(404);
+  die(mysqli_error($con));
+}
+
+if ($method == 'GET') {
+    if (!$id) echo '[';
+    for ($i=0 ; $i<mysqli_num_rows($result) ; $i++) {
+      echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
+    }
+    if (!$id) echo ']';
+  } elseif ($method == 'POST') {
+    echo json_encode($result);
+  } else {
+    echo mysqli_affected_rows($con);
+  }
+
+$con->close();
