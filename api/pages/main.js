fd0e945a6ae612d8680ce9b7a5ec9c9b3c19268f
zmiles1 <48224284+zmiles1@users.noreply.github.com>
2019-11-19 16:05:15 -0500

    Add files via upload

--- api/pages/main.js
+++ api/pages/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	$('#sidebarCollapse').on('click', function () {
             $('#sidebar').toggleClass('active')
         });
-	var store = "../store/getStores.php";
+	/*var store = "../store/getStores.php";
 	var bird = "../bird/selectBird.php";
 	var order = "../order/getOrders.php";
 	$.getJSON(store,function(storeData){
@@ -33,24 +33,24 @@ $(document).ready(function () {
                                 	orderStr+='<td>' + val.date + '</td>';
                                 	orderStr+= '<td class="text-center"><button class="btn btn-sm btn-outline-primary py-0" style="font-size: 0.8em;" type="button" onclick="removeOrder(' + val.id + ')">Remove #' + val.id + '</button></td></tr>';
                 		});
-				$('#orderTable').append(orderStr);
+				$('#ordertable').append(orderStr);
 			});
 		});
-	});
+	});*/
 
 		 
 
                   
-        hideAll();
-        $("#myInput").on("keyup", function() {
+        //hideAll();
+        /*$("#myInput").on("keyup", function() {
             var value = $(this).val().toLowerCase();
             $("#ordertable tr").filter(function() {
                 $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
             });
-        });
+        });*/
 });
 
-function hideAll(){
+/*function hideAll(){
         document.getElementById("form-group-2").style.display = "none";
         document.getElementById("form-group-3").style.display = "none";
         document.getElementById("form-group-4").style.display = "none";
@@ -85,10 +85,10 @@ function addOrderLine(){
         else {
                 alert("No further orders can be added");
         }
-}
+}*/
 
 
-function submitOrder(){
+/*function submitOrder(){
         if($('#deldate').val().match(/^(((0)[0-9])|((1)[0-2]))(\-)([0-2][0-9]|(3)[0-1])(\-)\d{2}$/i) && $('#numcoops').val() != ""){
                 addOrder($('#storeList').val(), $('#deldate').val(), $('#numcoops').val(), $('#bird1').val());
                 if(document.getElementById("form-group-2").style.display != "none" && $('#numcoops2').val() != ""){
@@ -154,5 +154,5 @@ function removeOrder(id){
         });
         alert("Removed order # " + id);
         window.location.reload();
-}
+}*/
 
