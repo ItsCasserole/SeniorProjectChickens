06e3d5363882a3c612995d9d9ee1b0ad5419c3d8
root <root@ip-172-31-48-133.ec2.internal>
2019-11-23 23:09:58 +0000

    Added permission checks for each page

--- api/pages/script.js
+++ api/pages/script.js
@@ -8,8 +8,32 @@ function logout(){
 }
 
 function loggedin(){
-    if(docCookies.getItem("userid") == null){
+    if(docCookies.getItem("userid") == null &&
+       window.location.href.indexOf("login.html") == -1){
 	window.location.href = "login.html";
+    }
+    else if(docCookies.getItem("userid") != null &&
+	    window.location.href.indexOf("login.html") != -1){
+	redirectOnPermissions();	
     }
 }
 
+function redirectOnPermissions(){
+    var perm = docCookies.getItem("permission");
+    if(perm == "Sales Manager" || perm == "Admin"){
+	window.location.href = "saleshomepage.html";
+    }
+    else if(perm == "Flock Manager"){
+	window.location.href = "FlockManager.html";
+    }
+    else if(perm == "Truck Driver"){
+	window.location.href = "truckdriver.html";
+    }
+}
+
+function checkPermissions(permissionNeeded){
+    var perm = docCookies.getItem("permission");
+    if(perm != permissionNeeded && perm != "Admin"){
+	redirectOnPermissions();
+    }
+}
