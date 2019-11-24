402765357453b8934ef0b278bbb4085ac2e9262b
root <root@ip-172-31-48-133.ec2.internal>
2019-11-24 06:17:07 +0000

    truckdriver messages

--- api/objects/message.php
+++ api/objects/message.php
@@ -7,64 +7,25 @@ class Message{
 
     // object properties
     public $message_id;
-    public $user_ID;
+    public $user_id;
     public $content;
-    public $timestamp;
+    public $date_created;
 
     // constructor with $db as database connection
     public function __construct($db){
         $this->conn = $db;
     }
 
-    // read farms
-    public function read(){
-        // call sql procedure to get query
-        $query = "Select * From Message";
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute();
-
-        return $stmt;
-    }
-
-    // create new farm
-    public function create(){
-        // call sql procedure to add new farm
-        $query = "INSERT INTO
-        " . $this->table_name . "
-    SET
-        content=:content";
-
-        $stmt = $this->conn->prepare($query);
-
-        // clean up inputs
-        $this->content=htmlspecialchars(strip_tags($this->content));
-
-
-        // bind values
-        $stmt->bindParam(":content", $this->content);
-
-        // execute query
-        if($stmt->execute()){
-            return true;
-        }
-
-        return false;
-    }
 
     //read messages for truck driver
-    public fuction readfortruckdriver(){
+    public function readfortruckdriver(){
 
      $sql = "CALL getmessagesforTD()";
      $stmt = $this->conn->prepare($sql);
-     if($stmt->execute()){
-            return true;
-        }
-
-        return false;
-
+     $stmt->execute();
+     return $stmt;
 
     }
-
 
 }
 ?>
